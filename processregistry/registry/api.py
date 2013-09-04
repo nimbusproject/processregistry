ed5080502010bf24d648a4c5cae275d5cdcf1ea1
Patrick Armstrong <patricka@uvic.ca>
2013-09-04 14:20:51 -0400

    Add per-user objects

--- processregistry/registry/api.py
+++ processregistry/registry/api.py
@@ -1,17 +1,28 @@
 import json
 
-from tastypie.authorization import Authorization
 from tastypie.authentication import BasicAuthentication
 from tastypie.resources import ModelResource
+
 from processregistry.registry.models import ProcessDefinition
+from processregistry.auth import UserObjectsOnlyAuthorization
 
 
 class ProcessDefinitionResource(ModelResource):
+
     class Meta:
         queryset = ProcessDefinition.objects.all()
         resource_name = 'process_definition'
-        authorization = Authorization()
+        authorization = UserObjectsOnlyAuthorization()
         authentication = BasicAuthentication()
+
+    def obj_create(self, bundle, **kwargs):
+        print "Creating with"
+        return super(ProcessDefinitionResource, self).obj_create(bundle, user=bundle.request.user)
+
+    def apply_authorization_limits(self, request, object_list):
+        print "LIMIT"
+        print request.user
+        return object_list.filter(user=request.user)
 
     def dehydrate_definition(self, bundle):
         return json.loads(bundle.data['definition'])
